89a895d817bb2b3d50fbe4646a5b61489cdfa62f
James Coulter <jamescoulter@BPAs-MacBook-Pro-2.local>
2016-02-18 19:48:30 -0500

    nav bar drop down

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+// $('.hamburger').click(function () {
+// $('.mobile-menu').css('top','70px');
+// });
+
+$('.hamburger').click(function () {
+if ( $('.mobile-menu').is(':hidden')){
+$('.mobile-menu').slideDown ('slow');
+} else {
+	$('.mobile-menu').slideUp();
+}
+});
